637ddf77861a310309fc847f9d344206c68104f2
Vistara agent <dev@vistara.dev>
2025-11-08 20:14:48 +0000

    Update: fix build errors i want to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+const { defineConfig } = require('vite')
+const react = require('@vitejs/plugin-react')
 
-export default defineConfig({
+module.exports = defineConfig({
   plugins: [react()],
 })
